d03cfc7e9205a4c16c3d50676ae931f3b49f1661
andresrestrepozapata21 <arz.950203@gmail.com>
2024-04-16 09:01:16 -0500

    actualzacion puerto permitirdo en CORS

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const app = express();
 const CURRENT_DIR = dirname(fileURLToPath(import.meta.url));
 // Configuring CORS options to include multiple sources
 const corsOptions = {
-    origin: ["http://localhost:3000", "http://127.0.0.1:5500"],
+    origin: ["http://localhost:3000", "http://127.0.0.1:5500", "http://127.0.0.1:5501"],
     methods: ["OPTIONS", "GET", "POST", "PUT", "DELETE"],
     credentials: true, // Allow cross-origin cookies
     allowedHeaders: ["Content-Type", "Authorization"]
